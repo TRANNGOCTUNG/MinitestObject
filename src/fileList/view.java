cb1bb7013ac22248f8e2dc38b27f84afd5895b56
FiveWing96 <117134067+FiveWing96@users.noreply.github.com>
2023-02-07 15:29:22 +0700

    second data

--- src/fileList/view.java
+++ src/fileList/view.java
@@ -1,2 +1,40 @@
-package fileList;public class view {
+package fileList;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class view {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("01", "Minh Ngọc", 23));
+        list.add(new Student("02", "Thu trang", 20));
+        writeToFile(list, "Demo.dat");
+        List<Student> studentList;
+        studentList = readFile("Demo.dat");
+        System.out.println(studentList);
+    }
+
+    public static void writeToFile(List<Student> students, String name) {
+        File file = new File(name);
+        try {
+            OutputStream os = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(students);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Student> readFile(String name) {
+        File file = new File(name);
+        try {
+            InputStream is = new FileInputStream(file);
+            ObjectInputStream oos = new ObjectInputStream(is);
+            List<Student> students = (List<Student>) oos.readObject();
+            return students;
+        } catch (IOException | ClassNotFoundException e) {
+            throw  new RuntimeException(e);
+        }
+    }
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,2 +1,35 @@
-package test;public class Test {
+package test;
+
+import fileList.Student;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("01","Minh Ngọc",23));
+        list.add(new Student("02", "Thu Trang",20));
+        writeFile(list);
+        try {
+            InputStream is = new FileInputStream("C:\\File Codegym\\New folder\\docs\\MinitestList\\input.txt");
+            ObjectInputStream ois = new ObjectInputStream(is);
+            OutputStream os = new FileOutputStream("C:\\File Codegym\\New folder\\docs\\MinitestList\\output.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            List<Student> students = (List<Student>) ois.readObject();
+            oos.writeObject(students);
+        } catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    public static void writeFile(List<Student> students){
+        try{
+            OutputStream os = new FileOutputStream("C:\\File Codegym\\New folder\\docs\\MinitestList\\input.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(students);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
